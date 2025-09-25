f13b0da686f054f80f85af39399b8197a6fe0ebb
AleRabazza <alejandrarabazza@gmail.com>
2025-09-24 21:45:26 -0300

    .

--- PiedraPapelOTijeras.Tests/JuegoTests.cs
+++ PiedraPapelOTijeras.Tests/JuegoTests.cs
@@ -13,6 +13,17 @@ namespace PiedraPapelOTijeras.Tests
             _jugador2 = new Jugador("Luis");
         }
 
+        [Theory]
+        [InlineData(1,3,3)]
+        [InlineData(2,3,3)]
+        [InlineData(4,3,3)]
+        public void ObtenerGanadorDeLaPartida(int puntaje2, int puntaje1, int total)
+        {
+           
+
+
+
+        }
 
     }
 }
